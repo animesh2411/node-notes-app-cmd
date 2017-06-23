2488566f9ffa5b750bb6e69157c2c50565d56a2d
Animesh Gupta <animesh2411@gmail.com>
2017-06-23 12:43:40 +0530

    First commit

---
+++ playground/debugging.js
@@ -0,0 +1,10 @@
+var person = {
+  name: 'Animesh'
+};
+
+person.age = 21;
+
+debugger;
+person.name= 'Kavya';
+
+console.log(person);
---
+++ playground/json.js
@@ -0,0 +1,25 @@
+// var obj = {
+//   name: 'Animesh'
+// };
+// var stringobj = JSON.stringify(obj);
+// console.log(typeof stringobj);
+// console.log(stringobj);
+//
+// var personString = '{"name":"Animesh", "age": 21}';
+// var person =JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some Body'
+};
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);
